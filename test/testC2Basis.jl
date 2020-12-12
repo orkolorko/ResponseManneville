f1ce2f46d58ff7f301b7b0b8f7b96249458ad054
Isaia Nisoli <nisoli@im.ufrj.br>
2020-12-12 16:04:51 -0300

    Added tests for the C1Norm

--- test/testC2Basis.jl
+++ test/testC2Basis.jl
@@ -22,5 +22,17 @@ for i in 1:N+1
     @test maximum(abs.(mid.(ν.(Interval.(B.p))) - zeros(N+1))) < 2^-15 
 end 
 
+v = zeros(length(B))
+v[1] = 1
+
+@test 1 ∈  ResponseManneville.C2BasisDefinition.infnormoffunction(B, v)
+@test 15*N/8 ∈ ResponseManneville.C2BasisDefinition.infnormofderivative(B, v)
+
+v = zeros(length(B))
+v[N+2] = 1
+
+@test 16/(81*N) ∈ ResponseManneville.C2BasisDefinition.infnormoffunction(B, v)
+@test 1 ∈ ResponseManneville.C2BasisDefinition.infnormofderivative(B, v)
+
 
 end;
