98133cd8d5a94a7e6106227cff27400e629b137c
Isaia Nisoli <nisoli@im.ufrj.br>
2021-01-12 13:17:27 -0300

    Now all the files for Ulam are here

--- src/NormsOfPowers.jl
+++ src/NormsOfPowers.jl
@@ -1,4 +1,37 @@
 """
+Functions to estimate Q|_{U^0}. See our paper for details.
+"""
+
+using LinearAlgebra
+using SparseArrays
+using FastRounding
+using ValidatedNumerics
+
+export norms_of_powers, refine_norms_of_powers, norms_of_powers_dfly, norms_of_powers_trivial, norms_of_powers_from_coarser_grid
+
+"""
+Returns the maximum number of (structural) nonzeros in a row of A
+"""
+function max_nonzeros_per_row(A::SparseMatrixCSC)
+    rows = rowvals(A)
+    m, n = size(A)
+    nonzeros_in_each_row = zeros(eltype(rows), m)
+    for i in rows
+        nonzeros_in_each_row[i] += 1
+    end
+    return maximum(nonzeros_in_each_row)
+end
+
+"""
+γₙ constants for floating point error estimation, as in [Higham, Accuracy and Stability of Numerical Algorithms]
+"""
+function gamma(T, n::Integer)
+    u = eps(T)
+    nu = u ⊗₊ T(n) #TODO: in theory, this should be rounded up/down. In practice, all integers up to 2^53 or so fit in Float64, so it won't be needed.
+    return nu ⊘₊ (one(T) ⊖₋ nu)
+end
+
+"""
 Estimates the norms ||Q||, ||Q^2||, ... ||Q^m|| on U^0.
 
 U is the matrix [ones(1,n-1); -I_(n-1,n-1)]. It is currently assumed that
@@ -35,15 +68,6 @@ function norms_of_powers(N::Type{<:NormKind}, m::Integer, Q::DiscretizedOperator
 
     nrmM = opnormbound(N, M)
 
-    if normQ == -1.
-        if is_integral_preserving(Q)
-            normQ = nrmM ⊕₊ δ
-        else
-            defect = opnormbound(N, Q.w)
-            normQ = nrmM ⊕₊ δ ⊕₊ normE ⊗₊ defect
-        end
-    end
-
     # precompute norms
     if !is_integral_preserving(Q)
         if normE == -1.
@@ -57,6 +81,15 @@ function norms_of_powers(N::Type{<:NormKind}, m::Integer, Q::DiscretizedOperator
         end
         if normN == -1.
             normN = opnormbound(N, Matrix(UniformScaling{Float64}(1),n,n) - Q.e*f)
+        end
+    end
+
+    if normQ == -1.
+        if is_integral_preserving(Q)
+            normQ = nrmM ⊕₊ δ
+        else
+            defect = opnormbound(N, Q.w)
+            normQ = nrmM ⊕₊ δ ⊕₊ normE ⊗₊ defect
         end
     end
 
@@ -95,4 +128,111 @@ function norms_of_powers(N::Type{<:NormKind}, m::Integer, Q::DiscretizedOperator
         end
     end
     return map(get_norm, normcachers)
+end
+
+"""
+Array of "trivial" bounds for the powers of a DiscretizedOperator (on the whole space)
+coming from from ||Q^k|| ≤ ||Q||^k
+"""
+function norms_of_powers_trivial(N::Type{<:NormKind}, Q::DiscretizedOperator, m::Integer)
+    norms = fill(NaN, m)
+    norms[1] = opnormbound(N, Q)
+    for i = 2:m
+        norms[i] = norms[i-1] ⊗₀ norms[1]
+    end
+    return norms
+end
+
+"""
+Arrays of bounds to ||Q^k||_{w → s} = sup_{||f||_w=1} ||Q^k f||_s
+and to ||Q^k||_{w}
+coming theoretically from iterated DFLY inequalities (the "small matrix method").
+
+Returns two arrays (strongs, norms) of length m:
+strongs[k] bounds ||Q^k f||_s, norms[k] bounds ||Q^k f||)
+"""
+function norms_of_powers_dfly(Bas::Basis, D::Dynamic, m)
+    A, B = dfly(strong_norm(Bas), aux_norm(Bas), D)
+    Eh = BasisDefinition.aux_normalized_projection_error(Bas)
+    M₁n = BasisDefinition.strong_weak_bound(Bas)
+    M₂ = BasisDefinition.aux_weak_bound(Bas)
+    S₁, S₂ = BasisDefinition.weak_by_strong_and_aux_bound(Bas)
+
+    norms = fill(NaN, m)
+    strongs = fill(NaN, m)
+
+    v = Array{Float64}([M₁n; M₂])
+    # We evaluate [S₁ S₂] * ([1 0; Eh 1]*[A B; 0 1])^k * [M₁n; M₂] (with correct rounding)
+    for k = 1:m
+        # invariant: v[1] bounds ||Q^kf||_s for ||f||_w=1
+        # v[2] bounds |||Q^kf||| for ||f||_w=1
+        v[1] = A ⊗₊ v[1] ⊕₊ B ⊗₊ v[2]
+        v[2] = Eh ⊗₊ v[1] ⊕₊ v[2]
+        strongs[k] = v[1]
+        norms[k] = S₁ ⊗₊ v[1] ⊕₊ S₂ ⊗₊ v[2]
+    end
+    return strongs, norms
+end
+
+
+"""
+Compute better and/or more estimates of power norms using
+the fact that ||Q^{k+h}|| ≤ ||Q^k|| * ||Q^h||.
+This uses multiplicativity, so it will not work for mixed norms,
+e.g., ||Q^k||_{s → w}, or ||M^k|_{U^0}||
+(unless M preserves U^0, which is the case for Q|_{U^0}).
+"""
+function refine_norms_of_powers(norms::Vector, m)
+    better_norms = fill(NaN, m)
+    better_norms[1] = norms[1]
+    for k in 2:m
+        better_norms[k] = minimum(better_norms[i] ⊗₊ better_norms[k-i] for i in 1:k-1)
+        if k <= length(norms)
+            better_norms[k] = min(better_norms[k], norms[k])
+        end
+    end
+    return better_norms
+end
+refine_norms_of_powers(norms::Vector) = refine_norms_of_powers(norms, length(norms))
+
+"""
+Estimate norms of powers from those on a coarser grid (see paper for details)
+"""
+function norms_of_powers_from_coarser_grid(fine_basis::Basis, coarse_basis::Basis, D::Dynamic, coarse_norms::Vector, normQ::Real)
+    if !BasisDefinition.is_refinement(fine_basis, coarse_basis)
+        @error "The fine basis is not a refinement of the coarse basis"
+    end
+    m = length(coarse_norms)
+    fine_norms = fill(NaN, m)
+    (strongs, norms) = norms_of_powers_dfly(fine_basis, D, m)
+
+    # adds a 0th element to strongs
+    strongs0(k::Integer) = k==0 ? BasisDefinition.strong_weak_bound(fine_basis) : strongs[k]
+    coarse_norms0(k::Integer) = k==0 ? 1. : coarse_norms[k]
+
+    Kh =  BasisDefinition.weak_projection_error(coarse_basis)
+    for k in 1:m
+		temp = 0.
+		for k in 0:m-1
+			temp = temp ⊕₊ coarse_norms0(m-1-k) ⊗₊ (normQ ⊗₊ strongs0(k) ⊕₊ strongs0(k+1))
+		end
+		fine_norms[k] = coarse_norms[k] ⊕₊ 2. ⊗₊ Kh ⊗₊ temp
+	end
+    return fine_norms
+end
+
+"""
+Estimate ||I + Q + Q^2 + … || (infinite sum) using a list of computed norm bounds norms[k] ≥ ||Q^k||.
+"""
+function infinite_sum_norms(norms::Vector)
+    m = length(norms)
+    if norms[m] > 1
+        @error "The last norm is > 1: the bounds are not sufficient to show that the sum converges"
+    end
+    S = 1.
+    for k = 1:m-1
+        S = S ⊕₊ norms[k]
+    end
+    S = S ⊘₊ (1. ⊖₋ norms[m])
+    return S
 end
--- src/ResponseManneville.jl
+++ src/ResponseManneville.jl
@@ -4,7 +4,13 @@ import IntervalArithmetic: Interval
 export Interval
 
 abstract type NormKind end
+
+struct L1 <: NormKind end
 struct Linf <: NormKind end
+struct Lipschitz <: NormKind end
+struct TotalVariation <: NormKind end
+struct ℓ1 <: NormKind end
+struct ℓinf <: NormKind end
 
 include("DynamicDefinition.jl")
 include("BasisDefinition.jl")
@@ -12,6 +18,7 @@ include("Contractors.jl")
 include("GenericAssembler.jl")
 include("GenericEstimate.jl")
 include("PwDynamicDefinition.jl")
+include("Norms.jl")
 include("NormsOfPowers.jl")
 include("UlamBasis.jl")
 include("C2Basis.jl")
